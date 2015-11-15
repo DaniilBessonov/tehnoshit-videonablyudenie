9355b311c0a3bd10a09e06929c570407c37a82ee
DaniilBessonov <danichbessonov@gmail.com>
2015-11-15 14:08:28 +0400

    add Email sending

---
+++ form_processor.php
@@ -0,0 +1,92 @@
+<?php
+if (isset($_POST['phone'])) {
+    $phone = $_POST['phone'];
+} else {
+    header('Location: fail.php?reason=Не%20указан%20телефон');
+}
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+} else {
+    header('Location: fail.php?reason=Не%20указано%20имя');
+}
+/*echo 'Phone: ' . $phone . '<br>';
+echo 'Name: ' . $name . '<br>';*/
+
+sendEmail("dabessonov@yandex.ru", $phone, $name);
+
+function sendEmail($address, $phone, $name)
+{
+    $mes = "Заказ обратного звонка!\nИмя клиента: $name\nТелефон: $phone\n\nСвяжитесь с клиентом как можно быстрее!";
+
+    $sub = 'Заказ обратного звонка';
+    //$email = 'call-back@videonablyudenie.info'; // от кого
+    $send = mail($address, $sub, $mes);
+    //$headers = 'From: webmaster@example.com';
+    //$send = mail($address, $sub, $mes, "Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+    //$send = mail($address, $sub, $mes, $headers);
+    //echo 'Email: ' . $send . '<br>';
+}
+
+ini_set('short_open_tag', 'On');
+header('Refresh: 3; URL=index.html');
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="3; url=index.html">
+    <title>Спасибо за заявку</title>
+    <link type="text/css" rel="stylesheet" href="css/global.css">
+    <style type="text/css">
+        body {
+            text-align: center;
+            background-color: rgb(130, 227, 74);
+            font-weight: 700;
+            font-size: 26px;
+        }
+
+        div#text-container {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            width: 300px;
+            height: 200px;
+            margin-top: -100px;
+            margin-left: -150px;
+        }
+
+        p {
+            font-size: 12px;
+            margin-top: 30px;
+        }
+    </style>
+</head>
+<body>
+<div id="text-container">
+    <h1>Спасибо за заявку!</h1>
+
+    <h2>С Вами скоро свяжутся</h2>
+
+    <p>Вы будете перенаправлены на главную страницу через <span>3</span> секунды</p>
+
+    <p><a href="index.html">На главную страницу</a></p>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+<script>
+    dec();
+
+    function dec() {
+        setTimeout(function () {
+            var val = $("span").html();
+            if (val > 1) {
+                $("span").html(val - 1);
+                dec();
+            } else {
+                location.replace("/index.html");
+            }
+        }, 1000);
+    }
+</script>
+
+</body>
+</html>
